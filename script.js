302c000b7fdd59bd4c595d7219d4f42265768a99
nguyendoanduylinh <nguyen.doan.duy.linh@gmail.com>
2022-05-18 11:29:29 +0700

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+function tangSo() {
+  let soNhap = parseInt(prompt("Nhap so can tang"));
+  let soSauTang = soNhap + 1;
+  alert(
+    `So vua nhap la ${soNhap}
+    So sau khi tang la ${soSauTang}`
+  );
+}
